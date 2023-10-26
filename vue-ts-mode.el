bc72f46a28c69a62abc6aa850acddff069f955fb
theschmocker <jacobcs1996@gmail.com>
2023-10-25 22:22:47 -0500

    adds / to the syntax table as punctuation

--- vue-ts-mode.el
+++ vue-ts-mode.el
@@ -522,6 +522,7 @@ and end tags."
   (modify-syntax-entry ?>  "." vue-ts-mode-syntax-table)
   (modify-syntax-entry ?<  "." vue-ts-mode-syntax-table)
   (modify-syntax-entry ?=  "." vue-ts-mode-syntax-table)
+  (modify-syntax-entry ?/  "." vue-ts-mode-syntax-table)
 
   (add-hook 'pre-command-hook #'vue-ts-mode--auto-open-tag-pre-command-h nil t)
   (add-hook 'post-command-hook #'vue-ts-mode--auto-close-tag-post-command-h nil t)
